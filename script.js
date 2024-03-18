14b53ff6a6b39b4bb9e795fa4dcc7968d721a3c0
manojkalyan27 <147662541+manojkalyan27@users.noreply.github.com>
2024-03-18 10:23:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let a = str.length;
+       
+       
+       for (let i = 0; i < a; i++) {
+           let count = 0;
+           for (let j = 0; j < a; j++) {
+               if(i !== j){
+                   if(str[i] == str[j]){
+                       count++;
+                       break;
+                   }	
+               }
+           }
+           if(count === 0){
+               console.log(str[i]);
+               break;
+           }
+       }
+       
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
